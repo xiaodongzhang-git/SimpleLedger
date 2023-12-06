b35df6a09b0afe1e7e407ef9df5c8ee0cc2822fd
zhangxiaodong <zhangxiaodong@rexceed.co.jp>
2023-12-06 10:07:40 +0900

    Added new Xcode project

---
+++ SimpleLedger/SimpleLedger/SimpleLedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleLedgerApp.swift
+//  SimpleLedger
+//
+//  Created by xiaodong zhang on 2023/12/06.
+//
+
+import SwiftUI
+
+@main
+struct SimpleLedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
